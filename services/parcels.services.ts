f3d289232a9c9369ed8097a73379de2a92c4f07a
Samad <eeysaad@nottingham.ac.uk>
2022-03-22 22:30:44 +0000

    part 4 complete and passing tests

--- services/parcels.services.ts
+++ services/parcels.services.ts
@@ -1,5 +1,5 @@
 
-import { SmallParcel, MediumParcel, LargeParcel, XLParcel, Item, SpeedyShipping } from '../models/parcel'
+import { SmallParcel, MediumParcel, LargeParcel, XLParcel, Item, SpeedyShipping, HeavyParcel } from '../models/parcel'
 interface parcelInput {
     xDimension: number,
     yDimension: number,
@@ -27,10 +27,27 @@ const ParcelsService = () => {
                         throw 'Error: dimensions must be numbers'
                     }
                     const parcelDimensions = xDimension * yDimension * zDimension
-                    if (parcelDimensions < 10) return new SmallParcel(weight)
-                    else if (parcelDimensions < 50) return new MediumParcel(weight)
-                    else if (parcelDimensions < 100) return new LargeParcel(weight)
-                    else return new XLParcel(weight)
+                    const heavyParcel = new HeavyParcel(weight)
+                    if (parcelDimensions < 10) {
+                        const smallParcel = new SmallParcel(weight)
+                        if (heavyParcel.cost < smallParcel.cost) return heavyParcel
+                        else return smallParcel
+                    }
+                    else if (parcelDimensions < 50) {
+                        const mediumParcel = new MediumParcel(weight)
+                        if (heavyParcel.cost < mediumParcel.cost) return heavyParcel
+                        else return mediumParcel
+                    }
+                    else if (parcelDimensions < 100) {
+                        const largeParcel = new LargeParcel(weight)
+                        if (heavyParcel.cost < largeParcel.cost) return heavyParcel
+                        else return largeParcel
+                    }
+                    else {
+                        const xlParcel = new XLParcel(weight)
+                        if (heavyParcel.cost < xlParcel.cost) return heavyParcel
+                        else return xlParcel
+                    }
                 })
                 if (speedyShipping) {
                     items.push(new SpeedyShipping(items))
